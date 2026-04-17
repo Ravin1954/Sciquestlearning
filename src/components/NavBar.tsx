33b91753ef6c0c4964ced91b20bda05233fa48dc
Sabita Sudhakaran <sabisudh@gmail.com>
2026-04-17 07:21:53 -0400

    Increase logo size in NavBar from 36px to 48px

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -18,7 +18,7 @@ export default function NavBar() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <Link href="/" className="flex items-center">
-            <img src="/logo.svg" alt="SciQuest Learning" style={{ height: '36px', width: 'auto' }} />
+            <img src="/logo.svg" alt="SciQuest Learning" style={{ height: '48px', width: 'auto' }} />
           </Link>
 
           <div className="hidden md:flex items-center gap-6">
